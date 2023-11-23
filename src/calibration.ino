812ea5b5f3c7c40934aee21a19e2c9d4c55a23bb
P3droSoares <pedropaulo0624@gmail.com>
2023-11-22 21:13:20 -0300

    feat: add calibration script

--- src/calibration.ino
+++ src/calibration.ino
@@ -3,9 +3,15 @@
 #define DOUT_PIN 2
 #define SCK_PIN 3
 
+float knowWeight = 200;
+
 void setup(){
     Serial.begin(9600);
     scale.begin(DOUT_PIN, SCK_PIN);
+
+    while(!Serial.available()){
+        delay(10);
+    }
 
     scale.set_scale();
 }
